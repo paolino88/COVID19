c844838eec340cd56544a0b58e300de5166160fb
paolino88 <paolino.iorio@eni.com>
2020-03-07 10:59:15 +0100

    commit1

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 def main():
 	""" A simple Iris EDA App """
 
-	st.title("Iris EDA App with streamlit")
-	st.subheader("Streamlit is Cool")
+	st.title("Statistic on COVID-19")
+	st.subheader("data from : https://github.com/CSSEGISandData/COVID-19")
 
 if __name__ == '__main__':
 	main()
